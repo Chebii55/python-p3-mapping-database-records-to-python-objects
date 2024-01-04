6e6448cb3d77d91864b6f79a2b2fd9ff82ceaa6d
Sheila Chebii <shillerchier055@gmail.com>
2024-01-04 13:42:14 +0300

    code along

--- lib/song.py
+++ lib/song.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-CONN = sqlite3.connect('music.db')
+CONN = sqlite3.connect('./music.db')
 CURSOR = CONN.cursor()
 
 class Song:
@@ -21,26 +21,25 @@ class Song:
                 album TEXT
             )
         """
-
-        CURSOR.execute(sql)
+        with CONN:
+            CURSOR.execute(sql)
 
     @classmethod
     def drop_table(cls):
         sql = """
             DROP TABLE IF EXISTS songs
         """
-
-        CURSOR.execute(sql)
+        with CONN:
+            CURSOR.execute(sql)
 
     def save(self):
         sql = """
             INSERT INTO songs (name, album)
             VALUES (?, ?)
         """
-
-        CURSOR.execute(sql, (self.name, self.album))
-
-        self.id = CURSOR.execute("SELECT last_insert_rowid() FROM songs").fetchone()[0]
+        with CONN:
+            CURSOR.execute(sql, (self.name, self.album))
+            self.id = CURSOR.lastrowid
 
     @classmethod
     def create(cls, name, album):
@@ -48,4 +47,57 @@ class Song:
         song.save()
         return song
 
-    # new code goes here!
+    @classmethod
+    def new_from_db(cls, row):
+        song = cls(row[1], row[2])
+        song.id = row[0]
+        return song
+
+    @classmethod
+    def all(cls):
+        sql = """
+            SELECT *
+            FROM songs
+        """
+        with CONN:
+            all_rows = CURSOR.execute(sql).fetchall()
+        return [cls.new_from_db(row) for row in all_rows]
+
+    @classmethod
+    def get_all(cls):
+        sql = """
+            SELECT *
+            FROM songs
+        """
+        with CONN:
+            all_rows = CURSOR.execute(sql).fetchall()
+        cls.all = [cls.new_from_db(row) for row in all_rows]
+        return cls.all
+
+    @classmethod
+    def find_by_name(cls, name):
+        sql = """
+            SELECT *
+            FROM songs
+            WHERE name = ?
+            LIMIT 1
+        """
+        with CONN:
+            song_row = CURSOR.execute(sql, (name,)).fetchone()
+        return cls.new_from_db(song_row)
+
+# Example usage:
+water = Song("Driving license", "Sour")
+nf = Song("Mistake", "Hope")
+barb = Song("Beef", "Barbie 2")
+
+# Create the table if it doesn't exist
+Song.create_table()
+
+# Save songs to the database
+water.save()
+nf.save()
+barb.save()
+
+# Retrieve all songs from the database
+all_songs = Song.get_
